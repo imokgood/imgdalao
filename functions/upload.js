e52f8f99b8856322912a5c2e284d254079f7054f
imokgood <105027930+imokgood@users.noreply.github.com>
2024-09-08 19:40:19 +0800

    Update upload.js

--- functions/upload.js
+++ functions/upload.js
@@ -9,7 +9,7 @@ export async function onRequestPost(context) {  // Contents of context object
      } = context;
      context.request
      const url = new URL(request.url);
-     const response = fetch('https://telegra.ph/' + url.pathname + url.search, {
+     const response = fetch('https://pic.518365.xyz/' + url.pathname + url.search, {
          method: request.method,
          headers: request.headers,
          body: request.body,
